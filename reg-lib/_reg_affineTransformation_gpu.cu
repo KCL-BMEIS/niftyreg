5567deb170e89b7dd466dfef7cab88a31cae8613
Marc Modat <m.modat@ucl.ac.uk>
2011-04-04 13:00:33 +0000

    Various texture were not unbinded. Thanks Stefano

--- reg-lib/_reg_affineTransformation_gpu.cu
+++ reg-lib/_reg_affineTransformation_gpu.cu
@@ -21,32 +21,32 @@ void reg_affine_positionField_gpu(	mat44 *affineMatrix,
 					nifti_image *targetImage,
 					float4 **array_d)
 {
-	int3 imageSize = make_int3(targetImage->nx,targetImage->ny,targetImage->nz);
-	CUDA_SAFE_CALL(cudaMemcpyToSymbol(c_ImageSize,&imageSize,sizeof(int3)));
-	CUDA_SAFE_CALL(cudaMemcpyToSymbol(c_VoxelNumber,&(targetImage->nvox),sizeof(int)));
+    int3 imageSize = make_int3(targetImage->nx,targetImage->ny,targetImage->nz);
+    CUDA_SAFE_CALL(cudaMemcpyToSymbol(c_ImageSize,&imageSize,sizeof(int3)));
+    CUDA_SAFE_CALL(cudaMemcpyToSymbol(c_VoxelNumber,&(targetImage->nvox),sizeof(int)));
 
-	// If the target sform is defined, it is used. The qform is used otherwise
-	mat44 *targetMatrix;
-	if(targetImage->sform_code>0)
-		targetMatrix=&(targetImage->sto_xyz);
-	else targetMatrix=&(targetImage->qto_xyz);
+    // If the target sform is defined, it is used. The qform is used otherwise
+    mat44 *targetMatrix;
+    if(targetImage->sform_code>0)
+            targetMatrix=&(targetImage->sto_xyz);
+    else targetMatrix=&(targetImage->qto_xyz);
 
-	// We here performed Affine * TargetMat * voxelIndex
-	// Affine * TargetMat is constant
-	mat44 transformationMatrix = reg_mat44_mul(affineMatrix, targetMatrix);
-	
-	// The transformation matrix is binded to a texture
-	float4 *transformationMatrix_h;
+    // We here performed Affine * TargetMat * voxelIndex
+    // Affine * TargetMat is constant
+    mat44 transformationMatrix = reg_mat44_mul(affineMatrix, targetMatrix);
+
+    // The transformation matrix is binded to a texture
+    float4 *transformationMatrix_h;
     CUDA_SAFE_CALL(cudaMallocHost(&transformationMatrix_h, 3*sizeof(float4)));
 	float4 *transformationMatrix_d;
     CUDA_SAFE_CALL(cudaMalloc(&transformationMatrix_d, 3*sizeof(float4)));
-	for(int i=0; i<3; i++){
-		transformationMatrix_h[i].x=transformationMatrix.m[i][0];
-		transformationMatrix_h[i].y=transformationMatrix.m[i][1];
-		transformationMatrix_h[i].z=transformationMatrix.m[i][2];
-		transformationMatrix_h[i].w=transformationMatrix.m[i][3];
-	}
-	CUDA_SAFE_CALL(cudaMemcpy(transformationMatrix_d, transformationMatrix_h, 3*sizeof(float4), cudaMemcpyHostToDevice));
+    for(int i=0; i<3; i++){
+            transformationMatrix_h[i].x=transformationMatrix.m[i][0];
+            transformationMatrix_h[i].y=transformationMatrix.m[i][1];
+            transformationMatrix_h[i].z=transformationMatrix.m[i][2];
+            transformationMatrix_h[i].w=transformationMatrix.m[i][3];
+    }
+    CUDA_SAFE_CALL(cudaMemcpy(transformationMatrix_d, transformationMatrix_h, 3*sizeof(float4), cudaMemcpyHostToDevice));
 	cudaBindTexture(0,txAffineTransformation,transformationMatrix_d,3*sizeof(float4));
     CUDA_SAFE_CALL(cudaFreeHost((void *)transformationMatrix_h));
 	
@@ -60,8 +60,9 @@ void reg_affine_positionField_gpu(	mat44 *affineMatrix,
     printf("[NiftyReg CUDA DEBUG] reg_affine_deformationField_kernel kernel: %s - Grid size [%i %i %i] - Block size [%i %i %i]\n",
 	       cudaGetErrorString(cudaGetLastError()),G1.x,G1.y,G1.z,B1.x,B1.y,B1.z);
 #endif
-	
-	CUDA_SAFE_CALL(cudaFree(transformationMatrix_d));
+
+    CUDA_SAFE_CALL(cudaUnbindTexture(txAffineTransformation));
+    CUDA_SAFE_CALL(cudaFree(transformationMatrix_d));
 }
 /* *************************************************************** */
 /* *************************************************************** */
