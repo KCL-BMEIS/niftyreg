1067a1c19e601562a883f2ebdd7a9e4a5e8409d2
Marc Modat <marc.modat@gmail.com>
2022-07-28 15:24:50 +0100

    85 need to update cmake files and cd environment (#87)
    * Issue #85: removal of mention of cdash has server is not available anymore
    
    * Issue #85: Adding a .ignore file
    
    * Issue #86 (#85): removed matlab dependent tests. Added a new one with Catch2 as a dependency
    
    * Issue #85 removing gitlab ci yaml file
    
    * Issue #85: Adding initial github action yaml file
    
    * Issue #85: Adding Catch2
    
    * Issue #85: Adding macos and win

--- reg-test/reg_test_affine_deformation_field.cpp
+++ reg-test/reg_test_affine_deformation_field.cpp
@@ -1,47 +1,151 @@
-#include "_reg_ReadWriteImage.h"
 #include "_reg_ReadWriteMatrix.h"
-#include "_reg_globalTrans.h"
 #include "_reg_tools.h"
 
 #include "Kernel.h"
 #include "AffineDeformationFieldKernel.h"
 #include "Platform.h"
 
+#include <catch2/catch_test_macros.hpp>
+
 #include "AladinContent.h"
 #ifdef _USE_CUDA
 #include "CUDAAladinContent.h"
 #endif
-
 #ifdef _USE_OPENCL
 #include "CLAladinContent.h"
 #endif
 
-#define EPS 0.000001
 #define EPS_SINGLE 0.0001
 
-void test(AladinContent *con, int platformCode) {
+TEST_CASE("Affine deformation field", "[Affine]") {
+    SECTION("2D") {
+        // Create a reference 2D image
+        int dim[8]= {2, 2, 2, 1, 1, 1, 1, 1};
+        nifti_image *reference = nifti_make_new_nim(
+                dim,
+                NIFTI_TYPE_FLOAT32,
+                true);
+        reg_checkAndCorrectDimension(reference);
 
-    Platform *platform = new Platform(platformCode);
+        // Generate the different use cases
+        typedef std::tuple<std::string, mat44 *, float *, float *> test_data;
+        std::vector<test_data> test_use_cases;
+        // Identity use case
+        auto *identity = new mat44;
+        reg_mat44_eye(identity);
+        // Test order [0,0] [1,0] [0,1] [1,1]
+        float identity_result_x[4] = {0, 1, 0, 1};
+        float identity_result_y[4] = {0, 0, 1, 1};
+        test_use_cases.emplace_back(test_data(
+                "- identity",
+                identity,
+                identity_result_x,
+                identity_result_y)
+        );
+        // Translation
+        auto *translation = new mat44;
+        reg_mat44_eye(translation);
+        translation->m[0][3] = -0.5;
+        translation->m[1][3] = 1.5;
+        // Test order [0,0] [1,0] [0,1] [1,1]
+        float translation_result_x[4] = {-0.5, .5, -0.5, .5};
+        float translation_result_y[4] = {1.5, 1.5, 2.5, 2.5};
+        test_use_cases.emplace_back(test_data(
+                "- translation",
+                translation,
+                translation_result_x,
+                translation_result_y)
+        );
 
-    Kernel *affineDeformKernel = platform->createKernel(AffineDeformationFieldKernel::getName(), con);
-    affineDeformKernel->castTo<AffineDeformationFieldKernel>()->calculate();
 
-    delete affineDeformKernel;
-    delete platform;
+        for(auto && test_use_case: test_use_cases) {
+
+            std::string test_name;
+            mat44 *test_mat;
+            float *test_res_x;
+            float *test_res_y;
+            std::tie(test_name, test_mat, test_res_x, test_res_y) = test_use_case;
+
+            SECTION(test_name) {
+                typedef std::tuple<AladinContent *, std::string, int> content_desc;
+                std::vector<content_desc> listContent;
+                // Compute the transformation field
+                listContent.push_back(content_desc(
+                        new AladinContent(
+                                reference,
+                                nullptr,
+                                nullptr,
+                                test_mat,
+                                sizeof(float)),
+                        "-- CPU",
+                        0));
+#ifdef _USE_CUDA
+                listContent.push_back(content_desc(
+                        new CudaAladinContent(
+                                reference,
+                                nullptr,
+                                nullptr,
+                                test_mat,
+                                sizeof(float)),
+                        "-- CUDA",
+                        1));
+#endif
+#ifdef _USE_OPENCL
+                listContent.push_back(content_desc(
+                        new ClAladinContent(
+                                reference,
+                                nullptr,
+                                nullptr,
+                                test_mat,
+                                sizeof(float)),
+                        "-- OpenCL",
+                        2));
+#endif
+                for (auto &&content: listContent) {
+
+                    AladinContent *con;
+                    std::string desc;
+                    int plat_value;
+                    std::tie(con, desc, plat_value) = content;
+
+                    SECTION(desc) {
+                        auto *platform = new Platform(plat_value);
+
+                        Kernel *affineDeformKernel = platform->createKernel(
+                                AffineDeformationFieldKernel::getName(),
+                                con);
+                        affineDeformKernel->castTo<AffineDeformationFieldKernel>()->calculate();
+
+                        nifti_image *defField =
+                                con->getCurrentDeformationField();
+
+                        // Check all values
+                        auto *defFieldPtrX = static_cast<float *>(defField->data);
+                        auto *defFieldPtrY = &defFieldPtrX[defField->nx *
+                                                           defField->ny];
+                        for (int i = 0; i < defField->nx*defField->ny; ++i) {
+                            REQUIRE(fabs(
+                                    defFieldPtrX[i] - test_res_x[i]) <
+                                    EPS_SINGLE);
+                            REQUIRE(fabs(
+                                    defFieldPtrY[i] - test_res_y[i]) <
+                                    EPS_SINGLE);
+                        }
+                        delete affineDeformKernel;
+                        delete platform;
+                    }
+                    delete con;
+                }
+                listContent.clear();
+            }
+            delete test_mat;
+        }
+        test_use_cases.clear();
+        nifti_image_free(reference);
+    }
 }
 
-int main(int argc, char **argv)
-{
-    if (argc != 5) {
-        fprintf(stderr, "Usage: %s <refImage> <inputMatrix> <expectedField> <platformCode>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    char *inputRefImageName = argv[1];
-    char *inputMatFileName = argv[2];
-    char *inputDefImageName = argv[3];
-    int platformCode = atoi(argv[4]);
-
+/*
     // Read the input reference image
     nifti_image *referenceImage = reg_io_ReadImageFile(inputRefImageName);
     if (referenceImage == NULL) {
@@ -127,4 +231,5 @@ int main(int argc, char **argv)
 
     return EXIT_SUCCESS;
 }
+ */
 
