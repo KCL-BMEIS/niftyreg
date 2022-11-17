21a038f3622955b92aaf844ba32a34af29b074b2
Marc Modat <marc.modat@gmail.com>
2022-11-17 20:22:43 +0000

    #86: Added resampling tests for identity transformation

--- reg-test/reg_test_interpolation.cpp
+++ reg-test/reg_test_interpolation.cpp
@@ -1,9 +1,13 @@
-#include "_reg_ReadWriteImage.h"
-#include "_reg_resampling.h"
+#include "_reg_ReadWriteMatrix.h"
 #include "_reg_tools.h"
 
+#include "Kernel.h"
 #include "ResampleImageKernel.h"
 #include "Platform.h"
+
+#include <list>
+#include <catch2/catch_test_macros.hpp>
+
 #include "AladinContent.h"
 #ifdef _USE_CUDA
 #include "CUDAAladinContent.h"
@@ -12,153 +16,189 @@
 #include "CLAladinContent.h"
 #endif
 
-#define EPS 0.000001
 #define EPS_SINGLE 0.0001
 
-void test(AladinContent *con, const unsigned int interp, int platformCode) {
-
-    Platform *platform = new Platform(platformCode);
-
-    Kernel *resampleImageKernel = platform->createKernel(ResampleImageKernel::getName(), con);
-    resampleImageKernel->castTo<ResampleImageKernel>()->calculate(interp, std::numeric_limits<float>::quiet_NaN());
-    //resampleImageKernel->castTo<ResampleImageKernel>()->calculate(interp, 0);
-
-    delete resampleImageKernel;
-    delete platform;
-}
+/*
+    This test file contains the following unit tests:
+    test function: image resampling
+    In 2D and 3D
+    identity
+    translation
+    affine
+*/
 
 
-int main(int argc, char **argv)
-{
-    if(argc!=6)
-    {
-        fprintf(stderr, "Usage: %s <floImage> <inputDefField> <expectedWarpedImage> <order> <platformCode>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+typedef std::tuple<std::string, nifti_image*, nifti_image*, float*> test_data;
+typedef std::tuple<AladinContent*, std::string, int> content_desc;
 
-    char *inputfloatingImageName=argv[1];
-    char *inputDefImageName=argv[2];
-    char *inputWarpedImageName=argv[3];
-    int interpolation=atoi(argv[4]);
-    int platformCode = atoi(argv[5]);
+TEST_CASE("Resampling", "[resampling]") {
+    // Create a reference 2D image
+    int dim[8] = { 2, 2, 2, 1, 1, 1, 1, 1 };
+    nifti_image* reference2D = nifti_make_new_nim(
+        dim,
+        NIFTI_TYPE_FLOAT32,
+        true);
+    reg_checkAndCorrectDimension(reference2D);
 
-    // Read the input floating image
-    nifti_image *floatingImage = reg_io_ReadImageFile(inputfloatingImageName);
-    if(floatingImage==NULL){
-        reg_print_msg_error("The input floating image could not be read");
-        return EXIT_FAILURE;
-    }
-    reg_tools_changeDatatype<float>(floatingImage);
-    // Read the input deformation field image image
-    nifti_image *inputDeformationField = reg_io_ReadImageFile(inputDefImageName);
-    if(inputDeformationField==NULL){
-        reg_print_msg_error("The input deformation field image could not be read");
-        return EXIT_FAILURE;
-    }
-    reg_tools_changeDatatype<float>(inputDeformationField);
-    // Read the input reference image
-    nifti_image *warpedImage = reg_io_ReadImageFile(inputWarpedImageName);
-    if(warpedImage==NULL){
-        reg_print_msg_error("The input warped image could not be read");
-        return EXIT_FAILURE;
-    }
-    //The expected warped image contains NaN, let's change them to quietNaN to be consitent
-    reg_tools_changeDatatype<float>(warpedImage);
-    float* wid = (float*) warpedImage->data;
-    for (size_t i = 0; i < warpedImage->nvox; i++) {
-        if (wid[i] != wid[i]) {
-            wid[i] = std::numeric_limits<float>::quiet_NaN();
+    // Fill image with distance from identity
+    auto* ref2dPrt = static_cast<float*>(reference2D->data);
+    for (float y = 0; y<reference2D->ny; ++y) {
+        for (float x = 0; x < reference2D->nx; ++x) {
+            *ref2dPrt = sqrtf(x*x + y*y);
+            ref2dPrt++;
         }
     }
 
-    // Check the dimension of the input images
-    if(warpedImage->nx != inputDeformationField->nx ||
-            warpedImage->ny != inputDeformationField->ny ||
-            warpedImage->nz != inputDeformationField->nz ||
-            (warpedImage->nz>1?3:2) != inputDeformationField->nu){
-        reg_print_msg_error("The input warped and deformation field images do not have corresponding sizes");
-        return EXIT_FAILURE;
-    }
-    if((floatingImage->nz>1) != (warpedImage->nz>1) ||
-            floatingImage->nt != warpedImage->nt){
-        reg_print_msg_error("The input floating and warped images do not have corresponding sizes");
-        return EXIT_FAILURE;
+    // Create a corresponding deformation field
+
+    // Create a reference 3D image
+    dim[0] = 3; dim[3] = 2;
+    nifti_image* reference3D = nifti_make_new_nim(
+        dim,
+        NIFTI_TYPE_FLOAT32,
+        true);
+    reg_checkAndCorrectDimension(reference3D);
+
+    // Fill image with distance from identity
+    auto* ref3dPrt = static_cast<float*>(reference3D->data);
+    for (float z = 0; z < reference3D->nz; ++z) {
+        for (float y = 0; y < reference3D->ny; ++y) {
+            for (float x = 0; x < reference3D->nx; ++x) {
+                *ref3dPrt = sqrtf(x * x + y * y + z * z);
+                ref3dPrt++;
+            }
+        }
     }
 
-    // Initialize a deformation field image
-    nifti_image *test_warped=nifti_copy_nim_info(warpedImage);
-    test_warped->data=(void *)malloc(test_warped->nvox*test_warped->nbyper);
-    //test_warped->data = (void *)calloc(test_warped->nvox, sizeof(float));
+    // Generate the different use cases
+    std::vector<test_data> test_use_cases;
 
-    //CPU - GPU code
-    int *tempMask = (int *)calloc(test_warped->nvox, sizeof(int));
+    // Identity use case - 2D
+    // First create an identity displacement field and then convert it into a deformation
+    nifti_image* id_field_2D = nifti_copy_nim_info(reference2D);
+    id_field_2D->ndim = id_field_2D->dim[0] = 5;
+    id_field_2D->nu = id_field_2D->dim[5] = 2;
+    id_field_2D->nvox = id_field_2D->nx * id_field_2D->ny * id_field_2D->nu;
+    id_field_2D->data = (void *)calloc(id_field_2D->nvox, id_field_2D->nbyper);
+    reg_getDeformationFromDisplacement(id_field_2D);
+    float res2[4];
+    memcpy(res2, reference2D->data, reference2D->nvox*sizeof(float));
+    // create the test case
+    test_use_cases.emplace_back(test_data(
+        "identity 2D",
+        reference2D,
+        id_field_2D,
+        res2)
+    );
 
-    AladinContent *con = NULL;
-    if (platformCode == NR_PLATFORM_CPU) {
-        con = new AladinContent(NULL, floatingImage, NULL, sizeof(float));
-    }
+    // Identity use case - 3D
+    nifti_image* id_field_3D = nifti_copy_nim_info(reference3D);
+    id_field_3D->ndim = id_field_3D->dim[0] = 5;
+    id_field_3D->nu = id_field_3D->dim[5] = 3;
+    id_field_3D->nvox = id_field_3D->nx * id_field_3D->ny * id_field_3D->nz * id_field_3D->nu;
+    id_field_3D->data = (void*)calloc(id_field_3D->nvox, id_field_3D->nbyper);
+    reg_getDeformationFromDisplacement(id_field_3D);
+    float res3[8];
+    memcpy(res3, reference3D->data, reference3D->nvox * sizeof(float));
+    // create the test case
+    test_use_cases.emplace_back(test_data(
+        "identity 3D",
+        reference3D,
+        id_field_3D,
+        res3)
+    );
+
+    // Loop over all generated test cases to create all content and run all tests
+    for (auto&& test_use_case : test_use_cases) {
+
+        // Retrieve test information
+        std::string test_name;
+        nifti_image *reference;
+        nifti_image *def_field;
+        float *test_res;
+        std::tie(test_name, reference, def_field, test_res) =
+            test_use_case;
+
+        // Accumate all required contents with a vector
+        std::vector<content_desc> listContent;
+        listContent.push_back(content_desc(
+            new AladinContent(
+                reference,
+                reference,
+                nullptr,
+                sizeof(float)),
+            "CPU",
+            NR_PLATFORM_CPU));
 #ifdef _USE_CUDA
-    else if (platformCode == NR_PLATFORM_CUDA) {
-        con = new CudaAladinContent(NULL, floatingImage, NULL, sizeof(float));
-    }
+        listContent.push_back(content_desc(
+            new CudaAladinContent(
+                reference,
+                reference,
+                nullptr,
+                sizeof(float)),
+            "CUDA",
+            NR_PLATFORM_CUDA));
 #endif
 #ifdef _USE_OPENCL
-    else if (platformCode == NR_PLATFORM_CL) {
-        con = new ClAladinContent(NULL, floatingImage, NULL, sizeof(float));
-    }
+        listContent.push_back(content_desc(
+            new ClAladinContent(
+                reference,
+                reference,
+                nullptr,
+                sizeof(float)),
+            "OpenCL",
+            NR_PLATFORM_CL));
 #endif
-    else {
-        reg_print_msg_error("The platform code is not suppoted");
-        return EXIT_FAILURE;
+        // Loop over all possibles contents for each test
+        for (auto&& content : listContent) {
+
+            AladinContent* con;
+            std::string desc;
+            int plat_value;
+            std::tie(con, desc, plat_value) = content;
+
+            SECTION(test_name + " " + desc) {
+                // Create and set a warped image to host the computation
+                nifti_image* warped = nifti_copy_nim_info(reference);
+                warped->data = (void*)malloc(warped->nvox * warped->nbyper);
+                con->setCurrentWarped(warped);
+                // Set the deformation field
+                con->setCurrentDeformationField(def_field);
+                // Set an empty mask to consider all voxels
+                int* tempMask = (int*)calloc(reference->nvox, sizeof(int));
+                con->setCurrentReferenceMask(tempMask, warped->nvox);
+                // Initialise the platform to run current content and retrieve deformation field
+                auto* platform = new Platform(plat_value);
+                Kernel* resampleKernel = platform->createKernel(
+                    ResampleImageKernel::getName(),
+                    con);
+                // args = interpolation and padding
+                std::list<int> interp = { 0, 1, 3 };
+                for (auto it : interp) {
+                    resampleKernel->castTo<ResampleImageKernel>()->calculate(
+                        it,
+                        0);
+                    warped = con->getCurrentWarped(reference->datatype);
+
+                    // Check all values
+                    auto* warpedPtr = static_cast<float*>(warped->data);
+                    for (int i = 0; i < warped->nx * warped->ny * warped->nz; ++i) {
+                        std::cout << i << " " << static_cast<float*>(reference->data)[i] << " " << warpedPtr[i] << " " << test_res[i] << std::endl;
+                        REQUIRE(fabs(
+                            warpedPtr[i] - test_res[i]) <
+                            EPS_SINGLE);
+                    }
+                }
+                delete resampleKernel;
+                delete platform;
+                free(tempMask);
+                delete con;
+            }
+        }
+        listContent.clear();
     }
-    //Check if the platform used is double capable
-    bool isDouble = con->isCurrentComputationDoubleCapable();
-    double proper_eps = EPS;
-    if(isDouble == 0) {
-        proper_eps = EPS_SINGLE;
-    }
-
-    con->setCurrentWarped(test_warped);
-    con->setCurrentDeformationField(inputDeformationField);
-    con->setCurrentReferenceMask(tempMask, test_warped->nvox);
-
-    test(con, interpolation, platformCode);
-    test_warped = con->getCurrentWarped(warpedImage->datatype);//check
-
-    // Compute the difference between the computed and inputed deformation field
-    nifti_image *diff_field = nifti_copy_nim_info(test_warped);
-    diff_field->data = (void *)malloc(diff_field->nvox*diff_field->nbyper);
-
-    // Compute the difference between the computed and inputed warped image
-    reg_tools_substractImageToImage(warpedImage, test_warped, diff_field);
-    reg_tools_abs_image(diff_field);
-    double max_difference = reg_tools_getMaxValue(diff_field, -1);
-
-#ifndef NDEBUG
-    if (max_difference > proper_eps) {
-        const char* tmpdir = getenv("TMPDIR");
-        char filename[255];
-        if(tmpdir!=NULL)
-            sprintf(filename, "%s/difference_warp_%iD_%i.nii", tmpdir, (diff_field->nz>1 ? 3 : 2), interpolation);
-        else sprintf(filename, "./difference_warp_%iD_%i.nii", (diff_field->nz>1 ? 3 : 2), interpolation);
-        reg_io_WriteImageFile(diff_field, filename);
-        reg_print_msg_error("Saving temp warped image:");
-        reg_print_msg_error(filename);
-    }
-#endif
-
-    nifti_image_free(floatingImage);
-    nifti_image_free(warpedImage);
-    nifti_image_free(inputDeformationField);
-    nifti_image_free(test_warped);
-
-    if(max_difference>proper_eps){
-        fprintf(stderr, "reg_test_interpolation error too large: %g (>%g)\n",
-                max_difference, proper_eps);
-        return EXIT_FAILURE;
-    }
-#ifndef NDEBUG
-    fprintf(stdout, "reg_test_interpolation ok: %g ( <%g )\n", max_difference, proper_eps);
-#endif
-    return EXIT_SUCCESS;
+    test_use_cases.clear();
+    // Only free-ing ref as the rest if cleared by content destructor
+    nifti_image_free(reference2D);
+    nifti_image_free(reference3D);
 }
