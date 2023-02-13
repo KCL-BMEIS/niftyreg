52cb0d74754af237fd8d309bb5b5b493cdfc1b57
Marc Modat <marc.modat@kcl.ac.uk>
2023-02-13 14:37:07 +0000

    Added default values for linear interpolation

--- reg-test/reg_test_interpolation.cpp
+++ reg-test/reg_test_interpolation.cpp
@@ -18,9 +18,8 @@
     This test file contains the following unit tests:
     test function: image resampling
     In 2D and 3D
-    identity
-    translation
-    affine
+    linear
+    cubic
 */
 
 
@@ -29,73 +28,99 @@ typedef std::tuple<std::unique_ptr<AladinContent>, std::unique_ptr<Platform>> Co
 
 TEST_CASE("Resampling", "[resampling]") {
     // Create a reference 2D image
-    int dim[8] = { 2, 2, 2, 1, 1, 1, 1, 1 };
-    nifti_image *reference2d = nifti_make_new_nim(dim, NIFTI_TYPE_FLOAT32, true);
+    int dim_flo[8] = { 2, 4, 4, 1, 1, 1, 1, 1 };
+    nifti_image *reference2d = nifti_make_new_nim(dim_flo, NIFTI_TYPE_FLOAT32, true);
     reg_checkAndCorrectDimension(reference2d);
 
     // Fill image with distance from identity
     auto* ref2dPrt = static_cast<float*>(reference2d->data);
-    for (float y = 0; y < reference2d->ny; ++y) {
-        for (float x = 0; x < reference2d->nx; ++x) {
-            *ref2dPrt = sqrtf(x * x + y * y);
+    for (auto y = 0; y < reference2d->ny; ++y) {
+        for (auto x = 0; x < reference2d->nx; ++x) {
+            *ref2dPrt = sqrtf(float(x * x) + float(y * y));
             ref2dPrt++;
         }
     }
 
-    // Create a corresponding deformation field
+    // Create a corresponding 2D deformation field
+    int dim_def[8] = {5, 1, 1, 1, 1, 2, 1, 1};
+    nifti_image *deformationField2D = nifti_make_new_nim(dim_def, NIFTI_TYPE_FLOAT32, true);
+    reg_checkAndCorrectDimension(deformationField2D);
+    auto* def2dPrt = static_cast<float*>(deformationField2D->data);
+    def2dPrt[0] = 1.2;
+    def2dPrt[1] = 1.3;
 
     // Create a reference 3D image
-    dim[0] = 3; dim[3] = 2;
-    nifti_image *reference3d = nifti_make_new_nim(dim, NIFTI_TYPE_FLOAT32, true);
+    dim_flo[0] = 3; dim_flo[3] = 4;
+    nifti_image *reference3d = nifti_make_new_nim(dim_flo, NIFTI_TYPE_FLOAT32, true);
     reg_checkAndCorrectDimension(reference3d);
 
     // Fill image with distance from identity
     auto *ref3dPrt = static_cast<float*>(reference3d->data);
-    for (float z = 0; z < reference3d->nz; ++z) {
-        for (float y = 0; y < reference3d->ny; ++y) {
-            for (float x = 0; x < reference3d->nx; ++x) {
-                *ref3dPrt = sqrtf(x * x + y * y + z * z);
+    for (auto z = 0; z < reference3d->nz; ++z) {
+        for (auto y = 0; y < reference3d->ny; ++y) {
+            for (auto x = 0; x < reference3d->nx; ++x) {
+                *ref3dPrt = sqrtf(float(x * x) + float(y * y) + float(z * z));
                 ref3dPrt++;
             }
         }
     }
 
+    // Create a corresponding 2D deformation field
+    dim_def[5] = 3;
+    nifti_image *deformationField3D = nifti_make_new_nim(dim_def, NIFTI_TYPE_FLOAT32, true);
+    reg_checkAndCorrectDimension(deformationField3D);
+    auto* def3dPrt = static_cast<float*>(deformationField3D->data);
+    def3dPrt[0] = 1.2;
+    def3dPrt[1] = 1.3;
+    def3dPrt[2] = 1.4;
+
     // Generate the different use cases
     std::vector<TestData> testCases;
 
-    // Identity use case - 2D
-    // First create an identity displacement field and then convert it into a deformation
-    nifti_image *idField2d = nifti_copy_nim_info(reference2d);
-    idField2d->ndim = idField2d->dim[0] = 5;
-    idField2d->nu = idField2d->dim[5] = 2;
-    idField2d->nvox = CalcVoxelNumber(*idField2d, idField2d->ndim);
-    idField2d->data = (void *)calloc(idField2d->nvox, idField2d->nbyper);
-    reg_getDeformationFromDisplacement(idField2d);
-    float res2[4];
-    memcpy(res2, reference2d->data, reference2d->nvox * sizeof(float));
+    // Linear interpolation - 2D
+    // coordinate in image: [1.2, 1.3]
+    auto *res_linear_2d = new float[1];
+    res_linear_2d[0] = 0;
+    for (auto y=1; y<2; ++y){
+        for (auto x=1; x<2; ++x){
+            res_linear_2d[0] += ref2dPrt[y*dim_flo[1]+
+                                         x] *
+                                abs(2.0 - (float)x - 0.2) *
+                                abs(2.0 - (float)y - 0.3);
+        }
+    }
+
     // create the test case
     testCases.emplace_back(TestData(
-        "identity 2D",
+        "Linear 2D",
         reference2d,
-        idField2d,
-        res2)
+        deformationField2D,
+        res_linear_2d)
     );
 
-    // Identity use case - 3D
-    nifti_image *idField3d = nifti_copy_nim_info(reference3d);
-    idField3d->ndim = idField3d->dim[0] = 5;
-    idField3d->nu = idField3d->dim[5] = 3;
-    idField3d->nvox = CalcVoxelNumber(*idField3d, idField3d->ndim);
-    idField3d->data = calloc(idField3d->nvox, idField3d->nbyper);
-    reg_getDeformationFromDisplacement(idField3d);
-    float res3[8];
-    memcpy(res3, reference3d->data, reference3d->nvox * sizeof(float));
+    // Linear interpolation - 23D
+    // coordinate in image: [1.2, 1.3, 1.4]
+    auto *res_linear_3d = new float[1];
+    res_linear_3d[0] = 0;
+    for (auto z=1; z<2; ++z){
+        for (auto y=1; y<2; ++y){
+            for (auto x=1; x<2; ++x) {
+                res_linear_3d[0] += ref2dPrt[z * dim_flo[1]* dim_flo[2] +
+                                             y * dim_flo[1] +
+                                             x] *
+                                    abs(2.0 - (float) x - 0.2) *
+                                    abs(2.0 - (float) y - 0.3) *
+                                    abs(2.0 - (float) z - 0.4);
+            }
+        }
+    }
+
     // create the test case
     testCases.emplace_back(TestData(
-        "identity 3D",
-        reference3d,
-        idField3d,
-        res3)
+            "Linear 3D",
+            reference3d,
+            deformationField3D,
+            res_linear_3d)
     );
 
     // Loop over all generated test cases to create all content and run all tests
