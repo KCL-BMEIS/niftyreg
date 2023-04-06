54a9990e471ce4883c2ff8017cf5aba987e6bc44
Onur Ülgen <onur.ulgen@kcl.ac.uk>
2023-04-06 13:02:02 +0100

    Use CPU for reg_f3d2::GetWarpedImage()

--- reg-lib/_reg_f3d2.cpp
+++ reg-lib/_reg_f3d2.cpp
@@ -57,6 +57,10 @@ T reg_f3d2<T>::InitCurrentLevel(int currentLevel) {
     nifti_image *reference, *floating;
     int *referenceMask, *floatingMask;
     if (currentLevel < 0) {
+        // Settings for GetWarpedImage()
+        // Use CPU for warping since CUDA isn't supporting Cubic interpolation
+        // TODO Remove this when CUDA supports Cubic interpolation
+        this->SetPlatformType(PlatformType::Cpu);
         reference = this->inputReference;
         floating = this->inputFloating;
         referenceMask = nullptr;
