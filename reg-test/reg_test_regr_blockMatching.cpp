9e85760519464fae50d8d32df1ea5b53a7da3299
Onur Ülgen <onur.ulgen@kcl.ac.uk>
2023-07-14 19:21:00 +0100

    Refactor the BlockMatching regression test

--- reg-test/reg_test_regr_blockMatching.cpp
+++ reg-test/reg_test_regr_blockMatching.cpp
@@ -3,28 +3,19 @@
 #include "CpuBlockMatchingKernel.h"
 #include "CudaBlockMatchingKernel.h"
 
-/*
-    This test file contains a regression test to ensure the CPU and GPU version yield the same output
-*/
+/**
+ *  Block matching regression test to ensure the CPU and CUDA versions yield the same output
+ */
 
 class BMTest {
-    /*
-    Class to call the block matching function
-    */
 protected:
     using TestData = std::tuple<std::string, NiftiImage, NiftiImage, int>;
-    using TestCase = std::tuple<std::string, _reg_blockMatchingParam *, _reg_blockMatchingParam *>;
+    using TestCase = std::tuple<std::string, unique_ptr<_reg_blockMatchingParam>, unique_ptr<_reg_blockMatchingParam>>;
+
     inline static vector<TestCase> testCases;
-    NiftiImage reference2d;
-    NiftiImage floating2d;
-    NiftiImage reference3d;
-    NiftiImage floating3d;
+
 public:
-    ~BMTest() {
-        std::cout << "Calling destructor" << std::endl;
-    }
     BMTest() {
-        std::cout << "Calling constructor" << std::endl;
         if (!testCases.empty())
             return;
 
@@ -34,110 +25,104 @@ public:
         std::uniform_real_distribution<float> distr(0, 1);
 
         // Create a reference and floating 2D images
-        NiftiImage::dim_t size = 64;
+        constexpr NiftiImage::dim_t size = 64;
         vector<NiftiImage::dim_t> dim{ size, size };
-        this->reference2d = NiftiImage(dim, NIFTI_TYPE_FLOAT32);
-        this->floating2d = NiftiImage(dim, NIFTI_TYPE_FLOAT32);
+        NiftiImage reference2d(dim, NIFTI_TYPE_FLOAT32);
+        NiftiImage floating2d(dim, NIFTI_TYPE_FLOAT32);
 
         // Create a reference 3D image
         dim.push_back(size);
-        this->reference3d = NiftiImage(dim, NIFTI_TYPE_FLOAT32);
-        this->floating3d = NiftiImage(dim, NIFTI_TYPE_FLOAT32);
+        NiftiImage reference3d(dim, NIFTI_TYPE_FLOAT32);
+        NiftiImage floating3d(dim, NIFTI_TYPE_FLOAT32);
 
         // Fill images with random values
-        float *ref2dPtr = static_cast<float *>(reference2d->data);
-        float *flo2dPtr = static_cast<float *>(floating2d->data);
+        const auto ref2dPtr = reference2d.data();
+        auto ref2dItr = ref2dPtr.begin();
+        const auto flo2dPtr = floating2d.data();
+        auto flo2dItr = flo2dPtr.begin();
         for (int y = 0; y < reference2d->ny; ++y)
             for (int x = 0; x < reference2d->nx; ++x) {
-                *ref2dPtr++ = distr(gen);
-                *flo2dPtr++ = distr(gen);
+                *ref2dItr++ = distr(gen);
+                *flo2dItr++ = distr(gen);
             }
 
         // Fill images with random values
-        float *ref3dPtr = static_cast<float *>(reference3d->data);
-        float *flo3dPtr = static_cast<float *>(floating3d->data);
+        const auto ref3dPtr = reference3d.data();
+        auto ref3dItr = ref3dPtr.begin();
+        const auto flo3dPtr = floating3d.data();
+        auto flo3dItr = flo3dPtr.begin();
         for (int z = 0; z < reference3d->nz; ++z)
             for (int y = 0; y < reference3d->ny; ++y)
                 for (int x = 0; x < reference3d->nx; ++x) {
-                    *ref3dPtr++ = distr(gen);
-                    *flo3dPtr++ = distr(gen);
+                    *ref3dItr++ = distr(gen);
+                    *flo3dItr++ = distr(gen);
                 }
-
 
         // Create the data container for the regression test
         vector<TestData> testData;
-        for(int b=50; b<=100; b+=50){
+        for (int b = 50; b <= 100; b += 50) {
             testData.emplace_back(TestData(
                 "BlockMatching 2D block " + std::to_string(b),
-                std::move(NiftiImage(this->reference2d)),
-                std::move(NiftiImage(this->floating2d)),
+                reference2d,
+                floating2d,
                 b
             ));
             testData.emplace_back(TestData(
                 "BlockMatching 3D block " + std::to_string(b),
-                std::move(NiftiImage(this->reference3d)),
-                std::move(NiftiImage(this->floating3d)),
+                reference3d,
+                floating3d,
                 b
             ));
         }
 
         for (auto&& data : testData) {
-            unique_ptr<Platform> platformCPU{ new Platform(PlatformType::Cpu) };
-            unique_ptr<Platform> platformCUDA{ new Platform(PlatformType::Cuda) };
-            // Make a copy of the test data
+            // Get the test data
             auto&& [testName, reference, floating, block] = data;
-            // Create content creator
-            unique_ptr<AladinContentCreator> contentCreatorCPU{
-                dynamic_cast<AladinContentCreator*>(platformCPU->CreateContentCreator(ContentType::Aladin))
-            };
-            unique_ptr<AladinContentCreator> contentCreatorCUDA{
-                dynamic_cast<AladinContentCreator*>(platformCUDA->CreateContentCreator(ContentType::Aladin))
-            };
-            // Create the contents
-            unique_ptr<AladinContent> contentCPU{ contentCreatorCPU->Create(
-                NiftiImage(reference).disown(),
-                NiftiImage(floating).disown(),
-                nullptr,
-                nullptr,
-                sizeof(float),
-                100,
-                block,
-                1
-            )};
-            unique_ptr<AladinContent> contentCUDA{ contentCreatorCUDA->Create(
-                NiftiImage(reference).disown(),
-                NiftiImage(floating).disown(),
-                nullptr,
-                nullptr,
-                sizeof(float),
-                100,
-                block,
-                1
-            )};
-            // Initialise the warped image
-            contentCPU->SetWarped(NiftiImage(floating).disown());
-            contentCUDA->SetWarped(NiftiImage(floating).disown());
-            // Initialise the block matching
-            std::unique_ptr<Kernel> kernelCPU = nullptr;
-            kernelCPU.reset(platformCPU->CreateKernel(BlockMatchingKernel::GetName(), contentCPU.get()));
-            std::unique_ptr<Kernel> kernelCUDA = nullptr;
-            kernelCUDA.reset(platformCUDA->CreateKernel(BlockMatchingKernel::GetName(), contentCUDA.get()));
 
-            // run the computation
-            kernelCPU->template castTo<CpuBlockMatchingKernel>()->Calculate();
-            kernelCUDA->template castTo<CudaBlockMatchingKernel>()->Calculate();
+            // Create images
+            NiftiImage referenceCpu(reference), referenceCuda(reference);
+            NiftiImage floatingCpu(floating), floatingCuda(floating);
+            NiftiImage warpedCpu(floating), warpedCuda(floating);
+
+            // Create the contents
+            unique_ptr<AladinContent> contentCpu{ new AladinContent(
+                referenceCpu,
+                floatingCpu,
+                nullptr,
+                nullptr,
+                sizeof(float),
+                100,
+                block,
+                1
+            ) };
+            unique_ptr<AladinContent> contentCuda{ new CudaAladinContent(
+                referenceCuda,
+                floatingCuda,
+                nullptr,
+                nullptr,
+                sizeof(float),
+                100,
+                block,
+                1
+            ) };
+
+            // Initialise the warped images
+            contentCpu->SetWarped(warpedCpu.disown());
+            contentCuda->SetWarped(warpedCuda.disown());
+
+            // Initialise the block matching
+            std::unique_ptr<BlockMatchingKernel> kernelCpu{ new CpuBlockMatchingKernel(contentCpu.get()) };
+            std::unique_ptr<BlockMatchingKernel> kernelCuda{ new CudaBlockMatchingKernel(contentCuda.get()) };
+
+            // Do the computation
+            kernelCpu->Calculate();
+            kernelCuda->Calculate();
 
             // Retrieve the information
-            _reg_blockMatchingParam *blockMatchingParamsCPU = new _reg_blockMatchingParam(contentCPU->GetBlockMatchingParams());
-            _reg_blockMatchingParam *blockMatchingParamsCUDA = new _reg_blockMatchingParam(contentCUDA->GetBlockMatchingParams());
+            unique_ptr<_reg_blockMatchingParam> blockMatchingParamsCpu{ new _reg_blockMatchingParam(contentCpu->GetBlockMatchingParams()) };
+            unique_ptr<_reg_blockMatchingParam> blockMatchingParamsCuda{ new _reg_blockMatchingParam(contentCuda->GetBlockMatchingParams()) };
 
-            testCases.push_back({
-                testName,
-                blockMatchingParamsCPU,
-                blockMatchingParamsCUDA
-            });
-            contentCPU.reset();
-            contentCUDA.reset();
+            testCases.push_back({ testName, std::move(blockMatchingParamsCpu), std::move(blockMatchingParamsCuda) });
         }
     }
 };
@@ -146,30 +131,25 @@ TEST_CASE_METHOD(BMTest, "Regression BlockMatching", "[regression]") {
     // Loop over all generated test cases
     for (auto&& testCase : this->testCases) {
         // Retrieve test information
-        auto&& [testName, blockMatchingParamsCPU, blockMatchingParamsCUDA] = testCase;
+        auto&& [testName, blockMatchingParamsCpu, blockMatchingParamsCuda] = testCase;
 
         SECTION(testName) {
+            std::cout << "\n**************** Section " << testName << " ****************" << std::endl;
 
-            // Ensure both approaches retreive the same number of voxel
-            REQUIRE(blockMatchingParamsCPU->activeBlockNumber==blockMatchingParamsCUDA->activeBlockNumber);
+            // Ensure both approaches retrieve the same number of voxels
+            REQUIRE(blockMatchingParamsCpu->activeBlockNumber == blockMatchingParamsCuda->activeBlockNumber);
 
             // Loop over the block and ensure all values are identical
-            for(int b=0; b<blockMatchingParamsCPU->activeBlockNumber*blockMatchingParamsCPU->dim; ++b){
-                float delta = blockMatchingParamsCPU->referencePosition[b] - blockMatchingParamsCUDA->referencePosition[b];
-                if(fabs(delta) > EPS){
-                    std::cout << "HERE " << delta << std::endl;
-                    std::cout.flush();
-                }
-                REQUIRE(fabs(delta) < EPS);
-                delta = blockMatchingParamsCPU->warpedPosition[b] - blockMatchingParamsCUDA->warpedPosition[b];
-                if(fabs(delta) > EPS){
-                    std::cout << "HERE " << delta << std::endl;
-                    std::cout.flush();
-                }
-                REQUIRE(fabs(delta) < EPS);
+            for (int b = 0; b < blockMatchingParamsCpu->activeBlockNumber * (int)blockMatchingParamsCpu->dim; ++b) {
+                const auto refPosCpu = blockMatchingParamsCpu->referencePosition[b];
+                const auto refPosCuda = blockMatchingParamsCuda->referencePosition[b];
+                std::cout << "referencePosition: " << b << " " << refPosCpu << " " << refPosCuda << std::endl;
+                REQUIRE(fabs(refPosCpu - refPosCuda) < EPS);
+                const auto warPosCpu = blockMatchingParamsCpu->warpedPosition[b];
+                const auto warPosCuda = blockMatchingParamsCuda->warpedPosition[b];
+                std::cout << "warpedPosition: " << b << " " << warPosCpu << " " << warPosCuda << std::endl;
+                REQUIRE(fabs(warPosCpu - warPosCuda) < EPS);
             }
-            delete blockMatchingParamsCPU;
-            delete blockMatchingParamsCUDA;
         }
     }
 }
