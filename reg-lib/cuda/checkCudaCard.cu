c3c9fdaf04fdb0c77bc64a539a502e6d72368d89
Onur Ülgen <onur.ulgen@kcl.ac.uk>
2024-02-05 15:32:14 +0000

    Update minimum CMake version and use the new CUDA method

--- reg-lib/cuda/checkCudaCard.cu
+++ reg-lib/cuda/checkCudaCard.cu
@@ -7,20 +7,18 @@ int main() {
     int deviceCount = 0, output = 0;
     const cudaError_t cudaResultCode = cudaGetDeviceCount(&deviceCount);
 
-    // Error when running cudaGetDeviceCount
     if (cudaResultCode != cudaSuccess) {
         std::cerr << cudaGetErrorString(cudaResultCode) << " (CUDA Error Code=" << cudaResultCode << ")" << std::endl;
         return EXIT_FAILURE;
     }
 
-    // Error when running cudaGetDeviceCount
     if (deviceCount == 0) {
         std::cerr << "No device detected" << std::endl;
         return EXIT_FAILURE;
     }
 
-    // Detect device capability and picks the best
-    for (unsigned i = 0; i < deviceCount; ++i) {
+    // Detect device capability and pick the best
+    for (int i = 0; i < deviceCount; i++) {
         cudaSetDevice(i);
         cudaDeviceProp deviceProp;
         cudaGetDeviceProperties(&deviceProp, i);
@@ -28,7 +26,7 @@ int main() {
     }
 
     // Output for device capability
-    std::cout << output;
+    std::cout << output / 10 << "." << output % 10;
 
     return EXIT_SUCCESS;
 }
